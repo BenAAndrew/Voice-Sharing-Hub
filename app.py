8488b92bb87466cef5dd75b964c48388f061aa69
BenAAndrew <bandrew01@qub.ac.uk>
2021-03-08 17:26:43 +0000

    remove url

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__, template_folder="static", static_folder="static")
 
 # Database
-DATABASE_PATH = "postgres://wtvkgdtoypnztx:3f55991b5f1dff58502fa50745a6ad971f0b386af5e66654092bc6929ca34109@ec2-176-34-222-188.eu-west-1.compute.amazonaws.com:5432/dfqauckl0k4ahq"
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_PATH
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
