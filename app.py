767ce898fa540610c1e5b3a00a37f95f1be34cd7
BenAAndrew <bandrew01@qub.ac.uk>
2021-03-08 17:19:33 +0000

    Update database url

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__, template_folder="static", static_folder="static")
 
 # Database
-DATABASE_PATH = "postgresql://postgres:password@localhost:5432/voicehub"
+DATABASE_PATH = "postgres://gzfvxmmqxnmflv:732c608384fbcdfeb81cd63201280200f000c3bdb562fc479bb0a48909e6abb6@ec2-176-34-222-188.eu-west-1.compute.amazonaws.com:5432/den28chhdor4ou"
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_PATH
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
